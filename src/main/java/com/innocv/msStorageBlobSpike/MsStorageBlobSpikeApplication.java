e44c92fb55fd2700d6535ea6c5d7afe1f1a5718e
Cristiam Sosa <cristiam.sosa@innocv.com>
2018-10-29 09:00:11 +0100

    Initial commit

---
+++ src/main/java/com/innocv/msStorageBlobSpike/MsStorageBlobSpikeApplication.java
@@ -0,0 +1,12 @@
+package com.innocv.msStorageBlobSpike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStorageBlobSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStorageBlobSpikeApplication.class, args);
+	}
+}
